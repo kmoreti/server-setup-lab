0bfabb33f532d10dd86d1fe15c3204194280354a
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-07 22:39:58 +0000

    initial commit

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,5 +1,4 @@
 cd /tmp || exit 1
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh /tmp/get-docker.sh
-sudo usermod -aG docker "$(logname)"
-echo "$(logname)"
+usermod -aG docker "$(logname)"
