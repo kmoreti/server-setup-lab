84c35ddd34c2beb2f6e120e7b3e6b2f8052e4166
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-07 21:08:04 +0000

    initial commit

--- setup.sh
+++ setup.sh
@@ -5,6 +5,16 @@ DOCKER_USER="kmserver"
 apt-get update
 apt-get upgrade -y
 
+is_curl_installed=$(curl -V)
+
+if [ -z "$is_curl_installed" ]
+then
+  apt-get install curl
+  echo "Curl is installed successfully."
+else
+  echo "Curl is already installed."
+fi
+
 is_docker_installed=$(docker --version)
 
 if [ -z "$is_docker_installed" ]
@@ -13,5 +23,15 @@ then
   echo "Docker is installed successfully."
 else
   echo "Docker is already installed."
+fi
+
+is_docker_compose_installed=$(docker-compose --version)
+
+if [ -z "$is_docker_compose_installed" ]
+then
+  docker/install-docker-compose.sh
+  echo "Docker-compose is installed successfully."
+else
+  echo "Docker-compose is already installed."
 fi
 
