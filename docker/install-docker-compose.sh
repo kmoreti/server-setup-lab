655b7c898ef31e656b947535408b34d894befd53
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-07 21:07:27 +0000

    initial commit

---
+++ docker/install-docker-compose.sh
@@ -0,0 +1,1 @@
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,13 +1,3 @@
-is_curl_installed=$(curl -V)
-
-if [ -z "$is_curl_installed" ]
-then
-  apt-get install curl
-  echo "Curl is installed successfully."
-else
-  echo "Curl is already installed."
-fi
-
 cd /tmp || exit 1
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh /tmp/get-docker.sh
