a0024fe2c8fe9bdba50dc593c913a5f4eb972341
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-07 20:58:11 +0000

    initial commit

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -3,6 +3,9 @@ is_curl_installed=$(curl -V)
 if [ -z "$is_curl_installed" ]
 then
   apt-get install curl
+  echo "Curl is installed successfully."
+else
+  echo "Curl is already installed."
 fi
 
 cd /tmp || exit 1
--- setup.sh
+++ setup.sh
@@ -10,5 +10,8 @@ is_docker_installed=$(docker --version)
 if [ -z "$is_docker_installed" ]
 then
   docker/install-docker.sh $DOCKER_USER
+  echo "Docker is installed successfully."
+else
+  echo "Docker is already installed."
 fi
 
