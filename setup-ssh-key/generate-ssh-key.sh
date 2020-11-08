5c5bbec47ac86722ccbe0945b3b95dd457b9e201
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-08 22:57:19 +0000

    changed generate-ssh-key.sh

--- setup-ssh-key/generate-ssh-key.sh
+++ setup-ssh-key/generate-ssh-key.sh
@@ -1,4 +1,4 @@
-if [ ! -f "$HOME/.ssh/id_rsa.pub" ]; then
+if [ ! -f "/home/$(logname)/.ssh/id_rsa.pub" ]; then
   echo "Generating ssh public key."
   sudo -u "$(logname)" ssh-keygen -t rsa -q -f "/home/$(logname)/.ssh/id_rsa" -N ""
 else
