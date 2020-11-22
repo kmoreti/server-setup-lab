e25d28803e44f1c3192c2e5b632c9aaf4bec1d6d
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-22 23:32:48 +0000

    removed script for distributing certificates

--- ca-and-tls/distribute-certificates.sh
+++
@@ -1,8 +0,0 @@
-CERT_DIR="ca-and-tls/certificates"
-
-for instance in master-1 master-2; do
-  scp "$CERT_DIR"/ca.crt "$CERT_DIR"/ca.key "$CERT_DIR"/kube-apiserver.key "$CERT_DIR"/kube-apiserver.crt \
-    "$CERT_DIR"/service-account.key "$CERT_DIR"/service-account.crt \
-    "$CERT_DIR"/etcd-server.key "$CERT_DIR"/etcd-server.crt \
-    ${instance}:~/
-done
