3c8f4125672483208ec1b74f3154393d479d544e
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-22 22:31:06 +0000

    adding script for distributing certificates

--- ca-and-tls/distribute-certificates.sh
+++ ca-and-tls/distribute-certificates.sh
@@ -1,4 +1,4 @@
-CERT_DIR=$(getent passwd "$(logname)" | cut -d: -f6)/server-setup-lab/ca-and-tls/certificates
+CERT_DIR="ca-and-tls/certificates"
 
 for instance in master-1 master-2; do
   scp "$CERT_DIR"/ca.crt "$CERT_DIR"/ca.key "$CERT_DIR"/kube-apiserver.key "$CERT_DIR"/kube-apiserver.crt \
--- setup-kubernetes-lab/setup-kubernetes-lab-environment.sh
+++ setup-kubernetes-lab/setup-kubernetes-lab-environment.sh
@@ -1,2 +1,3 @@
 cd "$1"/kubernetes-lab/Vagrant || exit 1
 sudo -u "$(logname)" vagrant up
+cd "$1"/server-setup-lab || exit 1
