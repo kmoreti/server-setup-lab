47d67979ba848ebab79627b91955ff05237c1a04
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-23 21:16:55 +0000

    added kubeconfig files

--- setup-kubernetes-lab/kubernetes/copy-kube-config.sh
+++ setup-kubernetes-lab/kubernetes/copy-kube-config.sh
@@ -1,5 +1,6 @@
-sudo -u "$(logname)" cp config "$1"/kubernetes-lab/Vagrant/kubernetes/
+LAB_KUBERNETES_DIR="$1"/kubernetes-lab/Vagrant/ubuntu/kubernetes
+sudo -u "$(logname)" cp config "$LAB_KUBERNETES_DIR"/
 
 LOADBALANCER_ADDRESS=192.168.5.30
 
-sudo -u "$(logname)" cat setup-kubernetes-lab/kubernetes/config-template/kube-proxy.kubeconfig_template > "$1"/kubernetes-lab/Vagrant/kubernetes/config/
+sudo -u "$(logname)" cat setup-kubernetes-lab/kubernetes/config-template/kube-proxy.kubeconfig_template | "$LAB_KUBERNETES_DIR"/config/
--- setup-kubernetes-lab/kubernetes/generate-encryption-config.sh
+++ setup-kubernetes-lab/kubernetes/generate-encryption-config.sh
@@ -1,6 +1,8 @@
+LAB_KUBERNETES_CONFIG_DIR="$1"/kubernetes-lab/Vagrant/ubuntu/kubernetes/config
+
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
-sudo -u "$(logname)" cat > "$1"/kubernetes-lab/Vagrant/kubernetes/config/encryption-config.yaml <<EOF
+sudo -u "$(logname)" cat > "$LAB_KUBERNETES_CONFIG_DIR"/encryption-config.yaml <<EOF
 kind: EncryptionConfig
 apiVersion: v1
 resources:
