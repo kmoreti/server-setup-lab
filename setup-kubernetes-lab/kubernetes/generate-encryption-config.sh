e381e57f057c8cbdec3e8837daba213a7a000fdd
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-23 22:34:56 +0000

    fixed generate-encryption-config.sh

--- setup-kubernetes-lab/kubernetes/generate-encryption-config.sh
+++ setup-kubernetes-lab/kubernetes/generate-encryption-config.sh
@@ -2,7 +2,7 @@ LAB_KUBERNETES_CONFIG_DIR="$1"/kubernetes-lab/Vagrant/ubuntu/kubernetes/config
 
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
-cat <<EOF sudo -u "$(logname)" tee "$LAB_KUBERNETES_CONFIG_DIR"/encryption-config.yaml > /dev/null
+cat <<EOF | sudo -u "$(logname)" tee "$LAB_KUBERNETES_CONFIG_DIR"/encryption-config.yaml > /dev/null
 kind: EncryptionConfig
 apiVersion: v1
 resources:
