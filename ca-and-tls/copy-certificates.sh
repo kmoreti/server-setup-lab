786517f772f963ea0a53ed113c655ed91902b7eb
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-23 21:33:11 +0000

    added kubeconfig files

--- ca-and-tls/copy-certificates.sh
+++ ca-and-tls/copy-certificates.sh
@@ -1,1 +1,3 @@
-sudo -u "$(logname)" cp -r ca-and-tls/certificates "$1"/kubernetes-lab/Vagrant/kubernetes/
+LAB_KUBERNETES_DIR="$1"/kubernetes-lab/Vagrant/ubuntu/kubernetes
+
+sudo -u "$(logname)" cp ca-and-tls/certificates "$LAB_KUBERNETES_DIR"
