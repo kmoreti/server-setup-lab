d3206b077e6e05b7162c98cd28bb0ff60ad9a614
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-11-23 21:47:02 +0000

    added kubeconfig files

--- setup-kubernetes-lab/kubernetes/copy-kube-config.sh
+++ setup-kubernetes-lab/kubernetes/copy-kube-config.sh
@@ -1,6 +1,6 @@
 LAB_KUBERNETES_DIR="$1"/kubernetes-lab/Vagrant/ubuntu/kubernetes
 sudo -u "$(logname)" cp -r setup-kubernetes-lab/kubernetes/config "$LAB_KUBERNETES_DIR"/
 
-LOADBALANCER_ADDRESS=192.168.5.30
+export LOADBALANCER_ADDRESS=192.168.5.30
 
 cat setup-kubernetes-lab/kubernetes/config-template/kube-proxy.kubeconfig_template | sudo -u "$(logname)" tee "$LAB_KUBERNETES_DIR"/config/kube-proxy.kubeconfig
