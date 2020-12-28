785bacc1f798d3f95bfa091ac2871038eb990f5f
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-28 19:37:56 +0000

    trying different version

--- setup-kubernetes-lab/kubernetes/download-kubernetes-binaries.sh
+++ setup-kubernetes-lab/kubernetes/download-kubernetes-binaries.sh
@@ -1,7 +1,12 @@
-sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-apiserver" -o "$KUBE_API_SERVER"
-sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-controller-manager" -o "$KUBE_CONTROLLER_MANAGER"
-sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-scheduler" -o "$KUBE_SCHEDULER"
-sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o "$KUBE_CTL"
+#sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-apiserver" -o "$KUBE_API_SERVER"
+#sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-controller-manager" -o "$KUBE_CONTROLLER_MANAGER"
+#sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kube-scheduler" -o "$KUBE_SCHEDULER"
+#sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o "$KUBE_CTL"
+
+sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-apiserver" -o "$KUBE_API_SERVER"
+sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-controller-manager" -o "$KUBE_CONTROLLER_MANAGER"
+sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-scheduler" -o "$KUBE_SCHEDULER"
+sudo -u "$(logname)" curl -fsSL "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl" -o "$KUBE_CTL"
 
 sudo -u "$(logname)" chmod +x "$KUBE_API_SERVER"
 sudo -u "$(logname)" chmod +x "$KUBE_CONTROLLER_MANAGER"
