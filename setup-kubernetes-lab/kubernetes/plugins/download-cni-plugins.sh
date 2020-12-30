cb23aa99e0120694dbfe058f2eac06f686a8ff4d
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2020-12-30 19:13:04 +0000

    added cni plugins download and etcd download

--- setup-kubernetes-lab/kubernetes/plugins/download-cni-plugins.sh
+++ setup-kubernetes-lab/kubernetes/plugins/download-cni-plugins.sh
@@ -1,2 +1,1 @@
-echo "Downloading CNI plugins..."
 sudo -u "$(logname)" curl -# -fSL "https://github.com/containernetworking/plugins/releases/download/v0.7.5/cni-plugins-amd64-v0.7.5.tgz" -o "$CNI_PLUGINS"
