fe53a4953576a8f790d4133e065f479ff960f2d8
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2021-01-18 23:09:29 +0000

    Adding scripts execution on nodes

--- setup.sh
+++ setup.sh
@@ -163,5 +163,5 @@ cd "$PROJECT"
 
 sudo -u "$(logname)" ssh -o "StrictHostKeyChecking no" vagrant@master-1 << EOF
 chmod +x "/home/vagrant/installation/scripts/install-master.sh"
-/home/vagrant/installation/scripts/install-master.sh
+sudo -u "$(logname)" /home/vagrant/installation/scripts/install-master.sh
 EOF
